6779cf66dc5226d56c1fad7cae57ebc0af3024ef
Justin Balgos <justino@go.byuh.edu>
2025-06-14 09:46:14 -1000

    sd

--- Activity-15/script.js
+++ Activity-15/script.js
@@ -1,3 +1,5 @@
-document.getElementById('changeColorButton').addEventListener('click', function() {
-    document.body.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+window.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('changeColorButton').addEventListener('click', function() {
+        document.body.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    });
 });
